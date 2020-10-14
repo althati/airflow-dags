81f033e73ceff3a81f3781adf8e91fe1ede718c7
Chandrashekar Althati <chandrashekar.althati@cioxhealth.com>
2020-10-14 05:00:49 -0400

    abcdd

--- parallel_dag.py
+++ parallel_dag.py
@@ -3,7 +3,7 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 
 from datetime import datetime
-
+#COMMENTS
 default_args = {
     'start_date': datetime(2019, 1, 1),
     'owner': 'Airflow',
@@ -14,8 +14,8 @@ def process(p1):
     return 'done'
 
 with DAG(dag_id='parallel_dag', schedule_interval='0 0 * * *', default_args=default_args, catchup=False) as dag:
-    
-    # Tasks dynamically generated 
+
+    # Tasks dynamically generated
     tasks = [BashOperator(task_id='task_{0}'.format(t), bash_command='sleep 60'.format(t)) for t in range(1, 4)]
 
     task_4 = PythonOperator(task_id='task_4', python_callable=process, op_args=['my super parameter'])
@@ -25,4 +25,3 @@ with DAG(dag_id='parallel_dag', schedule_interval='0 0 * * *', default_args=defa
     task_6 = BashOperator(task_id='task_6', bash_command='sleep 60')
 
     tasks >> task_4 >> task_5 >> task_6
-        
